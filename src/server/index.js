3ce6e239f53bbee4e6e4505871d8b7485a9af455
Alexey Dsiuba <atamur@yandex.ru>
2016-09-21 00:29:58 +0300

    GApps config

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
 app.use(express.static('target'));
-app.listen(8080, () => console.log('Server started'));
+app.listen(process.env.PORT || 8080, () => console.log('Server started'));
