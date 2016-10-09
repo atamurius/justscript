a21c468fba5f01a50b0cdea672fca5098f1e8ac9
Alexey Dsiuba <atamur@yandex.ru>
2016-10-10 00:28:03 +0300

    Code editor and redesign

--- src/client/index.js
+++ src/client/index.js
@@ -3,4 +3,5 @@ import React from 'react';
 import router from './router';
 import store from './store';
 
+window && (window.jQuery = require('jquery'));
 ReactDOM.render(router(store), document.getElementById('content'));
--- src/client/pages/test.jsx
+++ src/client/pages/test.jsx
@@ -3,11 +3,31 @@ import { Link } from 'react-router';
 import { increment, value } from 'client/modules/inc';
 import { listVersions } from 'client/modules/versions';
 import { connected } from 'client/utils/redux';
+import Ace from 'react-ace';
+import brace from 'brace';
+import 'brace/mode/javascript';
+import 'brace/theme/xcode';
+import $ from 'jquery';
 
 class Test extends React.Component {
 
   static stores = [ 'inc','versions' ];
   static actions = { increment };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.resizeEditors);
+    this.resizeEditors();
+  }
+
+  resizeEditors = () => {
+    $('.ace_editor').each((i,e) => {
+      this.resizeEditor($(e))
+    })
+  }
+
+  resizeEditor = e => {
+    e.css('width', e.parent().width());
+  }
 
   render() {
     const { increment } = this.props;
@@ -22,11 +42,16 @@ class Test extends React.Component {
           </button>
         </p>
         <h2>Changelog</h2>
-        <ul>
+        <ul className="text">
           {listVersions(this.props).map(v =>
             <li key={v.id}><code>{v.id}</code>&nbsp;&ndash; {v.description}</li>
           )}
         </ul>
+        <h2>Code editor</h2>
+        <div className="editor">
+          <Ace mode="javascript" theme="xcode" height="100px"
+               showPrintMargin={false} highlightActiveLine={true} />
+        </div>
       </div>
     );
   }
